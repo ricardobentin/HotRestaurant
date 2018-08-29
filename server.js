02a9ae38d2a071c011fe7eadc190aa4dfc073aff
Nano Bacano <ricardo.bentin@gmail.com>
2018-08-29 14:07:30 -0400

    able to view reservations and make reservations. need to work on clearing logic

---
+++ server.js
@@ -0,0 +1,74 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Reservations (DATA)
+// =============================================================
+let reservations = [
+  {
+    name: "Ricardo Bentin",
+    phone: "5128675309",
+    email: "rb@gmail.com",
+    uniqueID: "abc123"
+  },
+  {
+    name: "Jenna Bentin",
+    phone: "5555555555",
+    email: "jb@gmail.com",
+    uniqueID: "def567"
+  },
+];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+// get all tables - provides JSON
+app.get("/api/tables", function(req, res) {
+  for (var i = 0; i < reservations.length; i++) {
+    res.json(reservations);
+  }
+});
+
+// Create New reservation - takes in JSON input
+app.post("/api/tables", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newReservation = req.body;
+//   newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newReservation);
+
+  reservations.push(newReservation);
+
+  res.json(newReservation);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
